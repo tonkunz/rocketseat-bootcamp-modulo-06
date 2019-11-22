1f2ddcb89ebffce1f633bd656c1967c35ff3b80a
Everton Kunz <ehokeverton@gmail.com>
2019-11-22 00:14:42 -0300

    Adiciona Reactotron e React Navigation
    Adiciona StackNavigation, Main e User pages. Configura Reactotron...

--- src/index.js
+++ src/index.js
@@ -1,25 +1,10 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-
-  title: {
-    fontWeight: 'bold',
-  },
-});
+import './config/ReactotronConfig';
+import Routes from './routes';
 
 function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Hello App</Text>
-      <Text>React-Native Super App</Text>
-    </View>
-  );
+  return <Routes />;
 }
 
 export default App;
