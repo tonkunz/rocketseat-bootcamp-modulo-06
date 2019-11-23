ebcfb1513cfbc22cd6192352acd1fa53cb87d64e
Everton Kunz <ehokeverton@gmail.com>
2019-11-23 00:33:49 -0300

    Adiciona api do Github
    Adiciona a primeira chamada a api do github baseado no input do usuário

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,24 +1,60 @@
-import React from 'react';
-
+import React, { Component } from 'react';
+import { Keyboard } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import api from '../../services/api';
 
 import { Container, Form, Input, SubmitButton } from './styles';
 
-export default function Main() {
-  return (
-    <Container>
-      <Form>
-        <Input
-          autoCorrect={false}
-          autoCapitalize="none"
-          placeholder="Adicionar Usuário"
-        />
-        <SubmitButton>
-          <Icon name="add" size={20} color="#FFF" />
-        </SubmitButton>
-      </Form>
-    </Container>
-  );
+export default class Main extends Component {
+  state = {
+    newUser: '',
+    users: [],
+  };
+
+  handleSubmit = () => {
+    const { users, newUser } = this.state;
+
+    api.get(`/users/${newUser}`).then(({ data }) => {
+      const { name, login, bio, avatar } = data;
+
+      const user = {
+        name,
+        login,
+        bio,
+        avatar,
+      };
+
+      return this.setState({
+        users: [...users, user],
+        newUser: '',
+      });
+    });
+
+    Keyboard.dismiss();
+  };
+
+  render() {
+    const { users, newUser } = this.state;
+
+    return (
+      <Container>
+        <Form>
+          <Input
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Adicionar Usuário"
+            value={newUser}
+            onChangeText={text => this.setState({ newUser: text })}
+            returnKeyType="send"
+            onSubmitEditing={this.handleSubmit}
+          />
+          <SubmitButton onPress={this.handleSubmit}>
+            <Icon name="add" size={20} color="#FFF" />
+          </SubmitButton>
+        </Form>
+      </Container>
+    );
+  }
 }
 
 Main.navigationOptions = {
